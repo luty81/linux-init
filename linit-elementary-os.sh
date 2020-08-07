d69e2c663d32ab1c7d0ce226191e5f144fa75309
Luciano Fernandes <lucianocfernandes@gmail.com>
2020-08-07 20:00:47 -0300

    Fix package install ordering

--- linit-elementary-os.sh
+++ linit-elementary-os.sh
@@ -1,19 +1,17 @@
 #! /bin/bash
-
 echo '(1/4) Updating repositories...'
-apt-add-repository ppa:ubuntu-x-swat/updates
 apt update
 
 echo '(2/4) Installing essential tools...'
 apt install software-properties-common -y
 apt install net-tools -y
-apt install xsel -y
 apt install ssh -y
-apt install deepin-terminal -y
-apt install gitk -y
 apt install htop -y
+apt install xsel -y
+apt install deepin-terminal -y
 
 echo '(3/4) Installing generic drivers for Elementary OS...'
+apt-add-repository ppa:ubuntu-x-swat/updates
 apt install --install-recommends linux-generic-hwe-18.04 xserver-xorg-hwe-16.04 -y
 
 echo '(4/4) Cleaning everything...'
