4a9ba1aa30a339d6ec52f9c0a7fe5d1007630cb4
Luciano Fernandes <lucianocfernandes@gmail.com>
2020-08-08 20:29:09 -0300

    Added Linux Mint tools installing shell script.

--- linit-elementary-os.sh
+++ linit-elementary-os.sh
@@ -1,14 +1,18 @@
 #! /bin/bash
 echo '(1/4) Updating repositories...'
 apt update
+apt upgrade -y
 
 echo '(2/4) Installing essential tools...'
 apt install software-properties-common -y
 apt install net-tools -y
 apt install ssh -y
 apt install htop -y
+apt install sysstat -y
 apt install xsel -y
+apt install vim -y
 apt install deepin-terminal -y
+
 
 echo '(3/4) Installing generic drivers for Elementary OS...'
 apt-add-repository ppa:ubuntu-x-swat/updates
---
+++ linit-mint.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+apt update
+apt install sysstat -y
+apt install xsel -ytex
+apt install vim -y
+apt install deepin-terminal -y
+
+echo Done!
