9f4c73b1c62d0087cefbfc7a884d9f753df7dbe0
Luciano Fernandes <lucianocfernandes@gmail.com>
2020-08-10 00:33:04 -0300

    Added ssh key config to gitconfig script.

--- linit-gitconfig.sh
+++ linit-gitconfig.sh
@@ -10,3 +10,13 @@ if [ -n "$git_email" ]; then
 	git config --global user.email $git_email
 	git config --global user.name $git_name
 fi
+
+PUBLIC_KEY=/home/luciano/.ssh/id_rsa.pub
+if [ ! -f "$PUBLIC_KEY" ]; then
+	echo Generating SSH Key for Git Hub access...
+	ssh-keygen -t rsa -b 4096 -C "$git_email"
+	echo SSH Key generated successfully. 
+fi  
+
+echo Add the content below in https://github.com/settings/ssh/new
+cat ~/.ssh/id_rsa.pub
