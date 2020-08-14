7a13f8788106794b93957a70a0e294be345b794f
Luciano Fernandes <lucianocfernandes@gmail.com>
2020-08-14 01:01:31 -0300

    Added file watch limit increasing command

--- linit-mint.sh
+++ linit-mint.sh
@@ -5,4 +5,7 @@ apt install xsel -ytex
 apt install vim -y
 apt install deepin-terminal -y
 
+# Increases the default file watcher limit
+echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
+
 echo Done!
