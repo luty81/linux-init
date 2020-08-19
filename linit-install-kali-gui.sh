c7679060433481d02f354d0cfe7c7a166c373ebb
Luciano <lucianocfernandes@gmail.com>
2020-08-19 03:36:05 -0300

    Update linit-install-kali-gui.sh

--- linit-install-kali-gui.sh
+++ linit-install-kali-gui.sh
@@ -10,6 +10,10 @@ echo "[2/3] Installing Kex..."
 apt install kali-win-kex -y > /dev/null
 
 echo "[3/3] Installing Kali default package..."
-apt install  kali-linux-default -y > /dev/null
+apt install kali-linux-default -y > /dev/null
 
 echo "Done!"
+echo "In order to start Kali GUI run the follow command:"
+echo "\$ kex wtstart"
+echo "The GUI will be started in full screen and expanded to all monitors"
+echo "In order to run windowed instead, press F8 to show TigerVNC context menu and then uncheck Full Screen option"
