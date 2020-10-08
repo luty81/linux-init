d92b0ceecc86dcb1f83eb7740610094eca742191
Luciano <lucianocfernandes@gmail.com>
2020-10-08 10:55:38 -0300

    Delete macos-install-docker-toolbox.sh

--- macos-install-docker-toolbox.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-
-brew install docker-engine docker
-brew cask install virtualbox
-docker-machine create --driver virtualbox default
-eval "$(docker-machine env default)"
-docker-machine restart
-brew install docker-compose
